95b0bc4ac7bf01a6d834bf5d02d9077b16d85bca
ASuelvesE <angelsuelves@gmail.com>
2022-11-12 14:43:51 +0100

    Add colors

--- src/components/matches/item/MatchItem.jsx
+++ src/components/matches/item/MatchItem.jsx
@@ -1,7 +1,20 @@
 import "./match.css";
+import React, { useEffect, useState } from "react";
 
 export default function TeamItem({ result, position }) {
+  const [winnerA, setWinnerA] = useState([]);
+
   let fecha = new Date(result.datetime).toLocaleString();
+
+  useEffect(() => {
+    checkWinner();
+  }, []);
+
+  const checkWinner = () => {
+    if(result.pointsA > result.pointsB){
+      setWinnerA(true);
+    }else setWinnerA(false);
+  }
 
   return (
     <div className="match_item">
@@ -12,7 +25,7 @@ export default function TeamItem({ result, position }) {
           <img className="match_img_logo" src={result.teamAImage} alt={result.name} />
           <h3>{result.teamA}</h3>
         </div>
-        <p className="match_puntuacion"><b>{result.pointsA}</b></p>
+        <p className="match_puntuacion" style={{color: winnerA ? '#0AE800' : '#E81C00'}}><b>{result.pointsA}</b></p>
       </div>
 
 
@@ -21,7 +34,7 @@ export default function TeamItem({ result, position }) {
           <img className="match_img_logo" src={result.teamBImage} alt={result.name} />
           <h3>{result.teamB}</h3>
         </div>
-        <p className="match_puntuacion"><b>{result.pointsB}</b></p>
+        <p className="match_puntuacion" style={{color: !winnerA ? '#0AE800' : '#E81C00'}}><b>{result.pointsB}</b></p>
       </div>
 
     </div>
--- src/components/teams/item/TeamItem.jsx
+++ src/components/teams/item/TeamItem.jsx
@@ -1,13 +1,32 @@
 import "./team.css";
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 
 export default function TeamItem({ team, position }) {
+  const [color, setColor] = useState([]);
+
+  useEffect(() => {
+    colorPosition();
+  }, []);
+
+  const colorPosition = () => {
+    if(position === 1){
+      setColor("#0AE800");
+    }else if(position === 2){
+      setColor("#0074E8");
+    }else if(position === 3){
+      setColor("#E81C00");
+    }else {
+      setColor("black");
+    }
+  }
+
   return (
 
     <Link to={`/matches/${team.name}`}>
       <div className="team_item">
-        <h1>{position}º</h1>
+        <h1 style={{color: color}}>{position}º</h1>
         <div>
           <img src={team.image} alt={team.name} />
           <div>
@@ -16,7 +35,7 @@ export default function TeamItem({ team, position }) {
           </div>
         </div>
         <div className="points">
-          <h3><b>{team.points}</b></h3>
+          <h3 style={{color: color}}><b>{team.points}</b></h3>
         </div>
 
       </div>
